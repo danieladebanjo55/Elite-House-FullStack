1830c1c7f983012f6c0b39ef0429680bb9254c77
danieladebanjo55 <supremenftcoder@gmail.com>
2023-09-04 05:28:15 -0700

    docs: update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import Home from "./Components/Home/Home";
 import Success from "./Components/Success/Success";
 import Cancel from "./Components/Cancel/Cancel";
 import Benefit1 from "./Components/Home/Benefits/BenefitPage/Benefit1";
+import Benefit2 from "./Components/Home/Benefits/BenefitPage/Benefit2";
+import Benefit3 from "./Components/Home/Benefits/BenefitPage/Benefit3";
+import Benefit4 from "./Components/Home/Benefits/BenefitPage/Benefit4";
+import Benefit5 from "./Components/Home/Benefits/BenefitPage/Benefit5";
+import Benefit6 from "./Components/Home/Benefits/BenefitPage/Benefit6";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +22,11 @@ function App() {
           <Route path="/success" element={<Success />} />
           <Route path="/cancel" element={<Cancel />} />
           <Route path="/benefit1" element={<Benefit1 />} />
+          <Route path="/benefit2" element={<Benefit2 />} />
+          <Route path="/benefit3" element={<Benefit3 />} />
+          <Route path="/benefit4" element={<Benefit4 />} />
+          <Route path="/benefit5" element={<Benefit5 />} />
+          <Route path="/benefit6" element={<Benefit6 />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/Components/Home/Benefits/BenefitPage/Benefit1.jsx
+++ frontend/src/Components/Home/Benefits/BenefitPage/Benefit1.jsx
@@ -4,12 +4,15 @@ import "./Benefit1.scss";
 export default function Benefit1() {
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
-        consequatur animi fugiat dolore illo doloremque repellat. Laboriosam
-        quisquam enim, repellendus sit voluptatem, eum recusandae voluptates,
-        minus optio fugit atque commodi.
-      </p>
+      <div className="fig__container">
+        <div className="master__benefit">
+          <p className="master__context">
+            尚英社为会员提供实时账户管理和资金转账。该应用使会员可以开设多币种账户，包括人民币支付，有助于更轻松地在不同国
+            家进行财务管理，并实现即时跨境外汇和转账。成功申请后，每位客户都将拥有独有的新加坡IBAN国际银行号码，并能够进行
+            即时汇款和收款。所有尚英会会员的银行服务都受到新加坡金融管理局高度稳定的银行体系的保障。
+          </p>
+        </div>
+      </div>
     </>
   );
 }
