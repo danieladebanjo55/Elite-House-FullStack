d5e087d63cffed9501552c5c5c660e0f33da449e
danieladebanjo55 <supremenftcoder@gmail.com>
2023-09-04 09:53:29 -0700

    docs: update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Benefit3 from "./Components/Home/Benefits/BenefitPage/Benefit3";
 import Benefit4 from "./Components/Home/Benefits/BenefitPage/Benefit4";
 import Benefit5 from "./Components/Home/Benefits/BenefitPage/Benefit5";
 import Benefit6 from "./Components/Home/Benefits/BenefitPage/Benefit6";
+import Disclaimer from "./Components/Home/Footer/Disclaimer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,6 +28,7 @@ function App() {
           <Route path="/benefit4" element={<Benefit4 />} />
           <Route path="/benefit5" element={<Benefit5 />} />
           <Route path="/benefit6" element={<Benefit6 />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/Components/Home/Footer/Footer.jsx
+++ frontend/src/Components/Home/Footer/Footer.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Footer.scss";
 
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <>
       <div className="main__footer">
         <div className="footer__content">
-          <p>Powered by Buyzible. © 2023. All rights reserved</p>
+          <div className="link">
+            <Link to="/disclaimer">
+              <p className="loml">Disclaimer</p>
+            </Link>
+          </div>
+          <div className="paragraph">
+            <p>
+              Copyright &copy; 2023 Blockmind Technologies Ltd. Powered by
+              Buyzible
+            </p>
+          </div>
         </div>
       </div>
     </>
