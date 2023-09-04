6788d56f4813eb4ce15a128921290259694ba72a
danieladebanjo55 <supremenftcoder@gmail.com>
2023-09-03 22:54:28 -0700

    docs: update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Success from "./Components/Success/Success";
 import Cancel from "./Components/Cancel/Cancel";
+import Benefit1 from "./Components/Home/Benefits/BenefitPage/Benefit1";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/success" element={<Success />} />
           <Route path="/cancel" element={<Cancel />} />
+          <Route path="/benefit1" element={<Benefit1 />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/Components/Home/Benefits/BenefitPage/Benefit1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Benefit1.scss";
+
+export default function Benefit1() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+        consequatur animi fugiat dolore illo doloremque repellat. Laboriosam
+        quisquam enim, repellendus sit voluptatem, eum recusandae voluptates,
+        minus optio fugit atque commodi.
+      </p>
+    </>
+  );
+}
